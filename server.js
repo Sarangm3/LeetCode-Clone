1253f1b0202a3f2f9527a7800437da0d935b052d
Sarangm3 <sarangm3712@gmail.com>
2023-07-31 18:19:07 +0530

    deploy maybe final

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/api/users", require("./backend/routes/userRoutes"));
-
 app.use(express.static(path.join(__dirname, "./frontend/dist")));
 app.get("*", (req, res) =>
   res.sendFile(path.resolve(__dirname, "./", "frontend", "dist", "index.html"))
