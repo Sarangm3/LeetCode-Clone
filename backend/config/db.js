d0fcb79e97ee31259d1ceadf57cb587d14ae3616
Sarangm3 <sarangm3712@gmail.com>
2024-03-27 12:52:41 +0530

    deployment succefull

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,6 @@ mongoose.set("strictQuery", true);
 
 const connectDB = async () => {
   try {
-    if (process.env.MONGO_URI) {
-      console.log("MONGO_URI is set:", process.env.MONGO_URI);
-    } else {
-      console.log("MONGO_URI is not set");
-    }
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
